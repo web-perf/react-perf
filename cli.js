28cdf78f138f21087962ebb621cac648915e7918
Parashuram <code@nparashuram.com>
2013-09-28 13:31:11 -0700

    Added telemetry files

---
+++ cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var program = require('commander'),
+	telemetry = require('./lib/telemetry');
+
+program
+	.command('serve [page]')
+	.description('Serve telemetry [page]')
+	.action(function(env){
+		telemetry.serve(env);
+	});
+
+program
+	.command('run [component]')
+	.description('Run telemetry for component [component]')
+	.action(function(env){
+		telemetry.run(env)
+	});
+
+program
+	.version('0.0.1')
+	.parse(process.argv);
