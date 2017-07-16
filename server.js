2938e711f9c6e94d3ef2563d7d570c71b8f04518
Parashuram <code@nparashuram.com>
2017-07-15 21:53:04 -0700

    Changed static to node static for ES6

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var path = require('path');
 var fs = require('fs');
-var static = require('node-static');
+var nodeStatic = require('node-static');
 var prepareDBMonster = require('./dbmonster');
 
 var version = process.argv[2] || '0.14.0';
@@ -11,13 +11,13 @@ var binDir = process.argv[3] || path.join(__dirname, 'bin');
 
 try {
 	fs.mkdirSync(binDir);
-} catch (e) {}
+} catch (e) { }
 
-prepareDBMonster(version, binDir, function(resultDir) {
-	var server = require('http').createServer(function(request, response) {
-		request.addListener('end', function() {
-			new static.Server(resultDir).serve(request, response);
+prepareDBMonster(version, binDir, function (resultDir) {
+	var server = require('http').createServer(function (request, response) {
+		request.addListener('end', function () {
+			new nodeStatic.Server(resultDir).serve(request, response);
 		}).resume();
 	}).listen(8080);
-	console.log('Started server ' + resultDir);
+	console.log('Started server for ' + version + ' at ' + resultDir);
 });
